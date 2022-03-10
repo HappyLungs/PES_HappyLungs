c46657152f14df69c66cee771a84cd93d3b5db78
colotto7 <carlos.garcia.garcia@estudiantat.upc.edu>
2022-03-10 11:30:22 +0100

    prueba1

--- src/happyLungs/App.js
+++ src/happyLungs/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola</Text>
+      <Text>Hola </Text>
       <StatusBar style="auto" />
     </View>
   );
