4eeb08c0c4205dfc4faa89ca65c3ec346a991179
polkp22 <polkp22@gmail.com>
2022-03-10 14:38:22 +0100

    Add class Map

---
+++ src/app/domainLayer/classes/Map.js
@@ -0,0 +1,12 @@
+export default class Map {
+    title
+    constructor (name) {
+        this.title = name;
+    }
+    
+    //Getters
+    get MapTitle () {
+        return this.title;
+    }
+
+}
