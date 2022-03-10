8aa4335a6d9e4c7c502eb314de864ffdd5466409
colotto7 <carlos.garcia.garcia@estudiantat.upc.edu>
2022-03-10 15:20:09 +0100

    Pin.js added

---
+++ src/app/domainLayer/classes/Pin.js
@@ -0,0 +1,24 @@
+export default class Pin {
+    constructor(name, location, description, media, rating) {
+      this.name = name;
+      this.location = location;
+      this.description = description;
+      this.media = media;
+      this.rating = rating;
+    }
+  
+    create_pin(name, location, description, media, rating) {}
+    
+    //Getters
+    get recomended_IDs() {
+        return this.show_recomended_IDs;
+    }
+
+    get charts(){
+        return this.show_charts;
+    }
+
+    show_recomended_IDs(){}
+  
+    show_charts(){}
+  }
