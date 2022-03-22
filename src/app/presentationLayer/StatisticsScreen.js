539f89684fe56526e2bc973a1424fe073fea54ee
juliahc <julia.herrera.caba@estudiantat.upc.edu>
2022-03-22 01:19:04 +0100

    task #35 Veure estadístiques d'una localització

--- src/app/presentationLayer/StatisticsScreen.js
+++ src/app/presentationLayer/StatisticsScreen.js
@@ -1,5 +1,13 @@
 import React, { useRef, useState } from 'react';
-import { StyleSheet, View, Text, SafeAreaView, Platform, TouchableOpacity} from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, Platform, TouchableOpacity, Dimensions} from 'react-native';
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+  } from "react-native-chart-kit";
 
 import colors from '../config/stylesheet/colors';
 
@@ -24,39 +32,146 @@ function renderOptions() {
         </View>
     )
 }
-/*
-function renderChart() {
-    
-    let chartData = [ 526, 520, 470, 440, 250, 240, 170, 150, 120, 110, 80, 40 ]
-    let labels = ["NO2", "NO", "O3", "SO2", "CO", "NOX", "PM10", "H2S", "HCT", "PS", "HNCM", "C6H6"]
-    const reducer = (accumulator, curr) => accumulator+curr;
-    let totalPollution = chartData.reduce(reducer);
 
-    return (
+function renderLinearChart() {
+    const chartConfig = {
+        backgroundGradientFrom: "#1E2923",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#08130D",
+        backgroundGradientToOpacity: 0,
+        color: (opacity = 1) => "#8bc34a",
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+    };
+    const screenWidth = Dimensions.get("window").width;
+    const data = {
+        labels: ["00", "02", "04", "06", "08", "10", "12", "14", "16", "18", "20", "22"],
+        datasets: [
+          {
+            data: [2,2,2,2,3,3,3,4,4,3,2,2],
+            color: (opacity = 1) => "#4d4d4d", // optional
+            strokeWidth: 2 // optional
+          }
+        ],
+        //legend: ["Rainy Days"] // optional
+    };
+    return(
         <View style={{alignItems: 'center', justifyContent: 'center'}}>
-            <VictoryPie
-                data={chartData}
-                innerRadius={70}
-                //labels={chartData}
-                labels={labels}
-                style={{
-                    labels: { fontWeight: 'bold'},
-                    parent: {
-                        ...styles.shadow
-                    }
-                }}
-                width={350}
-                height={350}
-        />
-        <View style={{position: 'absolute', top: '42%', left: '41%'}}>
-            <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 15}}>
-                {totalPollution}</Text>
-            <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 15}}>Total</Text>
-        </View>
+            <LineChart
+                data={data}
+                width={screenWidth}
+                height={180}
+                chartConfig={chartConfig}
+                bezier
+            />
         </View>
     )
 }
-*/
+
+function renderPieChart() {
+    const data = [
+        {
+            name: "PM10 (µg/m3)",
+            quantity: 24,
+            color: "#F94144",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "NOX (µg/m3)",
+            quantity: 13,
+            color: "#F3722C",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "O3 (µg/m3)",
+            quantity: 57,
+            color: "#F8961E",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "SO2 (µg/m3)",
+            quantity: 1,
+            color: "#F9844A",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "H2S (µg/m3)",
+            quantity: 1.2,
+            color: "#F9C74F",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "CO  (mg/m3)",
+            quantity: 0.2,
+            color: "#90BE6D",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "NO (µg/m3)",
+            quantity: 2,
+            color: "#43AA8B",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "NO2 (µg/m3)",
+            quantity: 11,
+            color: "#4D908E",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        },
+        {
+            name: "PM2.5 (µg/m3)",
+            quantity: 19,
+            color: "#577590",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 13
+        }         
+    ];
+
+    data.sort((a,b) => (a.quantity < b.quantity));
+    let colorsPieChart = ["#F94144", "#F3722C", "#F8961E", "#F9844A", "#F9C74F", "#90BE6D", "#43AA8B", "#4D908E", "#4D908E", "#277DA1"];
+    for (let i = 0; i < data.length; i++) {
+        data[i].color = colorsPieChart[i];
+    }
+
+    const chartConfig = {
+        backgroundGradientFrom: "#1E2923",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#08130D",
+        backgroundGradientToOpacity: 0,
+        color: (opacity = 1) => "#8bc34a",
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+    };
+    
+    const screenWidth = Dimensions.get("window").width;
+
+    return(
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+            <PieChart
+                data={data}
+                width={screenWidth}
+                height={200}
+                chartConfig={chartConfig}
+                accessor={"quantity"}
+                backgroundColor={"transparent"}
+                paddingLeft={"0"}
+                center={[0, 0]}
+                absolute
+            />
+        </View>
+    )
+}
+
 function StatisticsScreen(props) {
 
     return (
@@ -67,12 +182,19 @@ function StatisticsScreen(props) {
             <View style={styles.stroke}/>
             <Text style={styles.subtitle}> Location-based data recorded over time</Text>
             {renderOptions()}
-            <Text style={styles.subtitle}> Pollutants to which you have been most exposed</Text>
-            <View style={styles.containerBtn}>
-                <View style={styles.CircleShape} />
-                <View style={styles.CircleShape} />
-                <View style={styles.CircleShape} />
+            {/*Com posar marges*/}
+            <View style={{justifyContent: "center", alignItems: "center", marginTop: 20, marginBottom: 10}} >
+                <Text style={styles.subtitle}> POLLUTION EVOLUTION</Text>
             </View>
+            <View style={{justifyContent: "center", alignItems: "center",}} >
+                {renderLinearChart()}
+            </View>
+            <View style={{justifyContent: "center", alignItems: "center", marginTop: 20, marginBottom: 10}} >
+                <Text style={styles.subtitle}> POLLUTANT QUANTITY</Text>
+            </View>
+            <View style={{justifyContent: "center", alignItems: "center",}} >
+                {renderPieChart()}
+            </View> 
     </SafeAreaView>        
     );      
 }
