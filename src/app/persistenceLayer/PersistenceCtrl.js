d01955e6f9491a28561d12dcbc8ee9f8d36aaa0c
IvanJimenoRamirez <ivan.jimeno@estudiantat.upc.edu>
2022-03-28 11:07:59 +0200

    task #42 Crear pin públic

--- src/app/persistenceLayer/PersistenceCtrl.js
+++ src/app/persistenceLayer/PersistenceCtrl.js
@@ -6,7 +6,7 @@ let PersistenceCtrl;
         instance = this;
 
         // initialize any properties of the singleton
-        this.DB_URI = "http://localhost:2000/v1";
+        this.DB_URI = "http://13.37.231.86:2000/v1";
     };
 }());
 
@@ -16,32 +16,82 @@ PersistenceCtrl.prototype.postRequest = async function (endpoint, params) {
     await axios({
         method: 'post',
         url: this.DB_URI + endpoint,
-        data: {
-            params
+        data: params,
+        headers: {
+            lfuzcudDtC36EFQW: "7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm",
+            authorization: "PES2022"
         }
       })
       .then(response => {
           res = response.data;
       })
       .catch(err => {
-          throw Error(err.code);
+          res = err;
       })
     return res;
 }
+
+
 PersistenceCtrl.prototype.getRequest = async function (endpoint, query) {
     let res = {};
     await axios({
         method: 'get',
         url: this.DB_URI + endpoint,
-        params: query
+        params: query,
+        headers: {
+            lfuzcudDtC36EFQW: "7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm",
+            authorization: "PES2022"
+        }
       })
       .then(response => {
           res = response.data;
       })
       .catch(err => {
-        throw Error(err.code);
+          res = err;
       })
     return res;
 }
 
+
+PersistenceCtrl.prototype.putRequest = async function (endpoint, params) {
+    let res = {};
+    await axios({
+        method: 'put',
+        url: this.DB_URI + endpoint,
+        data: params,
+        headers: {
+            lfuzcudDtC36EFQW: "7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm",
+            authorization: "PES2022"
+        }
+      })
+      .then(response => {
+          res = response.data;
+      })
+      .catch(err => {
+          res = err;
+      })
+    return res;
+}
+
+PersistenceCtrl.prototype.deleteRequest = async function (endpoint, query) {
+    let res = {};
+    await axios({
+        method: 'delete',
+        url: this.DB_URI + endpoint,
+        params: query,
+        headers: {
+            lfuzcudDtC36EFQW: "7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm",
+            authorization: "PES2022"
+        }
+        })
+        .then(response => {
+            res = response.data;
+        })
+        .catch(err => {
+            res = err;
+        })
+    return res;
+}
+
+        
 module.exports = PersistenceCtrl;
