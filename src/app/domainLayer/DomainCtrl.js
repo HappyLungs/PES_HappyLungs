853b80cfc11b14e175e4a91684dbad82afee1b93
alexs <alejandro.soriano@estudiantat.upc.edu>
2022-04-29 17:29:43 +0200

    Fetch Conversation by ID for DomainCtrl (to upgrade)

--- src/app/domainLayer/DomainCtrl.js
+++ src/app/domainLayer/DomainCtrl.js
@@ -1,5 +1,5 @@
 const DataPointMap = require("./classes/DataPointMap.js");
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 import Pin from "./classes/Pin";
 import User from "./classes/User";
@@ -10,13 +10,13 @@ const dadesObertes = new DadesObertes();
 
 let DomainCtrl;
 (function () {
-	let instance;
-	DomainCtrl = function () {
-		if (instance) return instance;
-		instance = this;
+  let instance;
+  DomainCtrl = function () {
+    if (instance) return instance;
+    instance = this;
 
-		// initialize any properties of the singleton
-	};
+    // initialize any properties of the singleton
+  };
 })();
 
 //MAP
@@ -26,37 +26,37 @@ let DomainCtrl;
  * @returns array with the contamination level of each measure station and its position
  */
 DomainCtrl.prototype.getMapData = async function () {
-	let date = new Date();
-	let measureStations = new Map();
-	let allMeasures = await dadesObertes.getMeasuresDate(date);
-	allMeasures.forEach((measure) => {
-		let eoiCode = measure.codi_eoi;
-		if (!measureStations.has(eoiCode)) {
-			let ms = new MeasureStation(
-				measure.codi_eoi,
-				measure.nom_estacio,
-				measure.tipus_estacio,
-				measure.latitud,
-				measure.longitud,
-				null
-			);
-			measureStations.set(eoiCode, ms);
-		}
-	});
+  let date = new Date();
+  let measureStations = new Map();
+  let allMeasures = await dadesObertes.getMeasuresDate(date);
+  allMeasures.forEach((measure) => {
+    let eoiCode = measure.codi_eoi;
+    if (!measureStations.has(eoiCode)) {
+      let ms = new MeasureStation(
+        measure.codi_eoi,
+        measure.nom_estacio,
+        measure.tipus_estacio,
+        measure.latitud,
+        measure.longitud,
+        null
+      );
+      measureStations.set(eoiCode, ms);
+    }
+  });
 
-	let measureStationLevels = [];
-	for (let [, ms] of measureStations) {
-		let level = await ms.getHourLevel(date, date.getHours());
-		if (level != null) {
-			let info = {
-				latitude: parseFloat(ms.latitud),
-				longitude: parseFloat(ms.longitud),
-				weight: parseFloat(level),
-			};
-			measureStationLevels.push(info);
-		}
-	}
-	return measureStationLevels;
+  let measureStationLevels = [];
+  for (let [, ms] of measureStations) {
+    let level = await ms.getHourLevel(date, date.getHours());
+    if (level != null) {
+      let info = {
+        latitude: parseFloat(ms.latitud),
+        longitude: parseFloat(ms.longitud),
+        weight: parseFloat(level),
+      };
+      measureStationLevels.push(info);
+    }
+  }
+  return measureStationLevels;
 };
 
 //STATISTICS - AIR QUALITY
@@ -68,24 +68,24 @@ DomainCtrl.prototype.getMapData = async function () {
  * @returns
  */
 DomainCtrl.prototype.getPollutionLevelLastDay = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	let data = await point.getDayLevel(date);
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  let data = await point.getDayLevel(date);
 
-	let finalTags = [];
-	let finalLevels = [];
-	for (let i = 1; i < 24; i += 2) {
-		finalTags.push(data.tags[i]);
-		finalLevels.push(data.levels[i]);
-	}
+  let finalTags = [];
+  let finalLevels = [];
+  for (let i = 1; i < 24; i += 2) {
+    finalTags.push(data.tags[i]);
+    finalLevels.push(data.levels[i]);
+  }
 
-	data.tags = finalTags;
-	data.levels = finalLevels;
+  data.tags = finalTags;
+  data.levels = finalLevels;
 
-	return data;
+  return data;
 };
 
 /**
@@ -95,83 +95,83 @@ DomainCtrl.prototype.getPollutionLevelLastDay = async function (
  * @returns
  */
 DomainCtrl.prototype.getPollutionLevelLastWeek = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	return await point.getWeekLevel(date);
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  return await point.getWeekLevel(date);
 };
 
 DomainCtrl.prototype.getPollutionLevelLastMonth = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	let data = await point.getMonthLevel(date);
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  let data = await point.getMonthLevel(date);
 
-	let finalTags = [];
-	let finalLevels = [];
-	for (let i = 0; i <= 30; i += 2) {
-		finalTags.push(data.tags[i]);
-		finalLevels.push(data.levels[i]);
-	}
+  let finalTags = [];
+  let finalLevels = [];
+  for (let i = 0; i <= 30; i += 2) {
+    finalTags.push(data.tags[i]);
+    finalLevels.push(data.levels[i]);
+  }
 
-	data.tags = finalTags;
-	data.levels = finalLevels;
+  data.tags = finalTags;
+  data.levels = finalLevels;
 
-	return data;
+  return data;
 };
 
 DomainCtrl.prototype.getPollutionLevelLastYear = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	data.tags = finalTags;
-	data.levels = finalLevels;
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  data.tags = finalTags;
+  data.levels = finalLevels;
 
-	return await point.getYearLevel(date);
+  return await point.getYearLevel(date);
 };
 
 //STATISTICS - POLLUTANTS
 
 DomainCtrl.prototype.getPollutantsQuantLastDay = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	return await point.getPollutantsQuantDay(date);
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  return await point.getPollutantsQuantDay(date);
 };
 
 DomainCtrl.prototype.getPollutantsQuantLastWeek = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	return await point.getPollutantsQuantWeek(date);
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  return await point.getPollutantsQuantWeek(date);
 };
 
 DomainCtrl.prototype.getPollutantsQuantLastMonth = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	return await point.getPollutantsQuantMonth(date);
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  return await point.getPollutantsQuantMonth(date);
 };
 
 DomainCtrl.prototype.getPollutantsQuantLastYear = async function (
-	latitude,
-	length
+  latitude,
+  length
 ) {
-	let date = new Date();
-	let point = new DataPointMap(latitude, length);
-	return await point.getPollutantsQuantYear(date);
+  let date = new Date();
+  let point = new DataPointMap(latitude, length);
+  return await point.getPollutantsQuantYear(date);
 };
 
 /**
@@ -185,15 +185,15 @@ DomainCtrl.prototype.getPollutantsQuantLastYear = async function (
  * @returns the created pin
  */
 DomainCtrl.prototype.createPin = function (
-	name,
-	location,
-	description,
-	media,
-	rating,
-	status
+  name,
+  location,
+  description,
+  media,
+  rating,
+  status
 ) {
-	return new Pin(name, location, description, media, rating, status);
-	//store db
+  return new Pin(name, location, description, media, rating, status);
+  //store db
 };
 
 /**
@@ -207,16 +207,16 @@ DomainCtrl.prototype.createPin = function (
  * @returns the updated pin
  */
 DomainCtrl.prototype.editPin = function (
-	name,
-	location,
-	description,
-	media,
-	rating,
-	status
+  name,
+  location,
+  description,
+  media,
+  rating,
+  status
 ) {
-	//edit not create
-	return new Pin(name, location, description, media, rating, status);
-	//store db
+  //edit not create
+  return new Pin(name, location, description, media, rating, status);
+  //store db
 };
 
 /**
@@ -228,21 +228,16 @@ DomainCtrl.prototype.editPin = function (
  * @param {*} birthdate
  * @returns message if error
  */
- DomainCtrl.prototype.registerUser = async function (
-	name,
-	email,
-	password,
-	confirmPassword,
-	birthdate
+DomainCtrl.prototype.registerUser = async function (
+  name,
+  email,
+  password,
+  confirmPassword,
+  birthdate
 ) {
-	//create
-	let myUser = new User(
-		name,
-		email,
-		password,
-		birthdate
-	);
-	return await myUser.register(confirmPassword);	//register to db
+  //create
+  let myUser = new User(name, email, password, birthdate);
+  return await myUser.register(confirmPassword); //register to db
 };
 
 /**
@@ -251,103 +246,94 @@ DomainCtrl.prototype.editPin = function (
  * @param {*} password
  * @returns an acces_token for the user
  */
- DomainCtrl.prototype.loginUser = async function (
-	email,
-	password
-) {
-	//create
-	let myUser = new User(
-		email,
-		password
-	);
-	return await myUser.login();	//login to db
+DomainCtrl.prototype.loginUser = async function (email, password) {
+  //create
+  let myUser = new User(email, password);
+  return await myUser.login(); //login to db
 };
 
+//Return the conversation with the id parameter.
 
-//Devolver todas las conversaciones
-DomainCtrl.prototype.fetchConversations = async function () {
-	//create
-	DB_URL = "http://localhost:7000/v1/conversation"
- var conver = [];
-	var users = {}
-	let conversations = await fetch(DB_URL)
-	.then(response => response.json())
-	
+/*
+ To update:
 
-	if(conversations.status === 200) {
-		for(var conversation in conversations.data){
-	
-			const logged = 	await this.findUser(conversations.data[conversation].users[0]);
-			const conversant = await this.findUser(conversations.data[conversation].users[1])
+  1. We dont have the function for logged user, so atm the first user in the array is the logged and the second the conversant
+  2. We need to check if the id is and mongoDB id. If the id doesnt exists we return empty object.
+  3. Dont have the Images ATM
 
-			 users = {
-				logged: {
-					id: logged.data._id,
-					name: logged.data.name,
-					profileImage: "null"
-				},
-				conversant: {
-					id: conversant.data._id,
-					name: conversant.data.name,
-					profileImage: "null"
-				}
-			};
-			
-			const messages = conversation.messages
-			const message = await this.findMessage("625ed69da994cd36f441fbe8");
-			conver.push({
-				id: message.data._id,
-				user: message.data.user,
-				text: message.data.text,
-				date: message.data.updatedAt
-			})
-		
-		
-		}
+*/
+DomainCtrl.prototype.fetchConversation = async function (id = null) {
+  //create
+  DB_URL = "http://localhost:7000/v1/conversation?_id=" + id;
+  var conver = [];
+  var users = {};
+  let conversation = await fetch(DB_URL).then((response) => response.json());
 
-	
-		
-		
+  if (conversation.status === 200) {
+    const logged = await this.findUser(conversation.data.users[0]);
+    const conversant = await this.findUser(conversation.data.users[1]);
 
-	}
-	return ({users: await users, messages: conver})
+    users = {
+      logged: {
+        id: logged.data._id,
+        name: logged.data.name,
+        profileImage: "null",
+      },
+      conversant: {
+        id: conversant.data._id,
+        name: conversant.data.name,
+        profileImage: "null",
+      },
+    };
+
+    const messages = conversation.messages;
+    const message = await this.findMessage("625ed69da994cd36f441fbe8");
+    conver.push({
+      id: message.data._id,
+      user: message.data.user,
+      text: message.data.text,
+      date: message.data.updatedAt,
+    });
+  }
+  return { users:  users, messages: conver };
 };
 
 //Devolver todas las conversaciones
 DomainCtrl.prototype.findUser = async function (email) {
-	//create
-	DB_URL = "http://localhost:7000/v1/user?email=" + email
+  //create
+  DB_URL = "http://localhost:7000/v1/user?email=" + email;
 
-	
-	let user = await fetch(DB_URL, {
-		headers: {
-			'Accept': 'application/json',
-					  'Content-Type': ' application/json',
-					  'X-Api-Key': '7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm'
-				  },
-	})
-	.then(response => response.json())
-	.then(data => data)
+  let user = await fetch(DB_URL, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": " application/json",
+      "X-Api-Key":
+        "7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data);
 
-	return user;
-	console.log(user);
+  return user;
+  console.log(user);
 };
 
 DomainCtrl.prototype.findMessage = async function (id) {
-	//create
-	DB_URL = "http://localhost:7000/v1/message?_id=" + id
+  //create
+  DB_URL = "http://localhost:7000/v1/message?_id=" + id;
 
-	let message = await fetch(DB_URL, {
-		headers: {
-			'Accept': 'application/json',
-					  'Content-Type': ' application/json',
-					  'X-Api-Key': '7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm'
-				  },
-	})
-	.then(response => response.json())
-	.then(data => data)
-	return message;
-	console.log(user);
+  let message = await fetch(DB_URL, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": " application/json",
+      "X-Api-Key":
+        "7j7C1I1vy46tpgwUybXt4y4tMlIVXKUSSQiHo73K1X3f3pZpoKHg7BzJK5sxEddkRmR3hID7vwcm",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+  return message;
+  console.log(user);
 };
 
 /**
@@ -360,22 +346,22 @@ DomainCtrl.prototype.findMessage = async function (id) {
  * @returns the updated user
  */
 DomainCtrl.prototype.updateUser = function (
-	username,
-	email,
-	points,
-	healthState,
-	profilePicture
+  username,
+  email,
+  points,
+  healthState,
+  profilePicture
 ) {
-	//edit, not create
-	let updatedUser = new User(
-		username,
-		email,
-		points,
-		healthState,
-		profilePicture
-	);
-	return updatedUser;
-	//update to db
+  //edit, not create
+  let updatedUser = new User(
+    username,
+    email,
+    points,
+    healthState,
+    profilePicture
+  );
+  return updatedUser;
+  //update to db
 };
 
 module.exports = DomainCtrl;
--- src/app/domainLayer/classes/test.js
+++ src/app/domainLayer/classes/test.js
@@ -1,11 +1,9 @@
-
 const DomainCtrl = require("../DomainCtrl.js");
 
-
-async function test(){
-    test = new DomainCtrl();
-    const x = await test.fetchConversations();
-    console.log(x);
+async function test() {
+  test = new DomainCtrl();
+  const x = await test.fetchConversation("626bfe2c8c2231ddb23ee15c");
+  console.log(x);
 }
 
 test();
