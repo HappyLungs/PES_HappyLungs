4a3b30f3c301dd0098993604fa639d5d6192e556
juliahc <julia.herrera.caba@estudiantat.upc.edu>
2022-05-04 19:12:14 +0200

    task #153 : GeneralPinScreen refactor

--- src/app/presentationLayer/GeneralChatScreen.js
+++ src/app/presentationLayer/GeneralChatScreen.js
@@ -8,6 +8,8 @@ import {
 	TouchableOpacity,
 	ScrollView,
 	FlatList,
+	Image,
+	Dimensions
 } from "react-native";
 
 import COLORS from "../config/stylesheet/colors";
@@ -16,7 +18,13 @@ import i18n from "../config/translation";
 const PresentationCtrl = require("./PresentationCtrl.js");
 
 import { MaterialIcons } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
 import * as Animatable from "react-native-animatable";
+
+
+
+
+
 
 function GeneralChatScreen({ navigation }) {
 	let presentationCtrl = new PresentationCtrl();
@@ -24,17 +32,9 @@ function GeneralChatScreen({ navigation }) {
 	const [masterData, setMasterData] = useState([]);
 	const [filteredData, setFilteredData] = useState([]);
 	const [auxiliarFilterData, setAuxiliarFilterData] = useState([]);
-	const [auxiliarFilterData2, setAuxiliarFilterData2] = useState([]);
+	const [modalDeleteVisible, setModalDeleteVisible] = useState(false);
 	const [search, setSearch] = useState("");
-	const [dateFilter, setDateFilter] = useState(true);
-	const [ratingFilter, setRatingFilter] = useState(false);
-	const [createdFilter, setCreatedFilter] = useState(false);
-	const [savedFilter, setSavedFilter] = useState(false);
-
 	const AnimationRefFilter1 = useRef(null);
-	const AnimationRefFilter2 = useRef(null);
-	const AnimationRefFilter3 = useRef(null);
-	const AnimationRefFilter4 = useRef(null);
 
 	useEffect(() => {
 		fetchChats();
@@ -67,6 +67,188 @@ function GeneralChatScreen({ navigation }) {
 			setFilteredData(masterData);
 		}
 		setSearch(text);
+	};
+
+	function renderChatList (chatsList) {
+		const renderItem = ({ item, index }) => (
+			<Animatable.View animation="slideInDown" duration={500} delay={index * 10}>
+				<View
+					style={[
+						{
+							borderBottomColor: COLORS.light,
+							borderBottomWidth: 1,
+							backgroundColor: COLORS.white,
+						},
+					]}
+				>
+					<View style={{ flexDirection: "column" }}>
+						<View style={{ flexDirection: "row" }}>
+							<View
+								style={{
+									width: "85%",
+									padding: 10,
+								}}
+							>
+								<TouchableOpacity
+									style={{
+										flexDirection: "row",
+										flex: 1,
+										alignItems: "center",
+									}}
+									onPress={() => {
+										navigation.navigate("ChatConversation", {id: item.id});
+									}}
+								>
+									<Image
+										source={{ uri: item.profileImage }}
+										style={{
+											width: 70,
+											height: 70,
+											borderRadius: 100,
+										}}
+									/>
+									<View
+										style={{
+											flex: 1,
+											marginTop: 10,
+											marginBottom: 10,
+											marginLeft: 5,
+											alignSelf: "center",
+											flexDirection: "row",
+											width: "auto",
+										}}
+									>
+										<View style={{}}>
+											<View
+												style={{
+													flexDirection: "row",
+													justifyContent: "space-between",
+													alignItems: "flex-start",
+													paddingStart: 5,
+												}}
+											>
+												<View
+													style={{
+														alignSelf: "flex-start",
+														padding: 2,
+														width: "75%",
+													}}
+												>
+													<Text
+														numberOfLines={1}
+														style={[styles.chatName, { textAlign: "left" }]}
+													>
+														{item.name}
+													</Text>
+												</View>
+												<View
+													style={{
+														alignSelf: "flex-end",
+														padding: 2,
+													}}
+												>
+													<Text
+														style={[
+															styles.chatName,
+															{
+																color: COLORS.darkGrey,
+																fontSize: 12,
+																fontWeight: "bold",
+																textAlign: "right",
+															},
+														]}
+													>
+														{item.lastMessageTime}
+													</Text>
+												</View>
+											</View>
+											<View
+												style={{
+													flexDirection: "row",
+													justifyContent: "flex-start",
+													alignItems: "center",
+												}}
+											>
+												<View
+													style={{
+														width: "90%",
+													}}
+												>
+													<Text numberOfLines={2} style={styles.chatLastMessage}>
+														{item.lastMessage}
+													</Text>
+												</View>
+	
+												<View
+													style={{
+														backgroundColor:
+															item.unreadMessages > 0
+																? COLORS.green1
+																: COLORS.white,
+														width: 20,
+														height: 20,
+														paddingBottom: 1,
+														justifyContent: "center",
+														borderRadius: 100,
+													}}
+												>
+													<Text
+														style={[
+															styles.chatName,
+															{
+																color: COLORS.white,
+																fontSize: 12,
+																fontWeight: "bold",
+															},
+														]}
+													>
+														{item.unreadMessages > 0 ? item.unreadMessages : ""}
+													</Text>
+												</View>
+											</View>
+										</View>
+									</View>
+								</TouchableOpacity>
+							</View>
+							<View
+								style={{
+									justifyContent: "center",
+									alignItems: "center",
+									borderLeftColor: COLORS.lightGrey,
+									borderLeftWidth: 1,
+									width: "15%",
+								}}
+							>
+								<TouchableOpacity
+									onPress={() => {
+										/*navigation.navigate("ChatConversation");*/
+									}}
+								>
+									<EvilIcons
+										name="trash"
+										style={styles.sendIcon}
+										color={COLORS.red1}
+										size={35}
+									/>
+								</TouchableOpacity>
+							</View>
+						</View>
+					</View>
+				</View>
+			</Animatable.View>
+		);
+	
+		return (
+			<FlatList
+				stickyHeaderHiddenOnScroll={true}
+				contentContainerStyle={{}}
+				scrollEnabled={true}
+				data={chatsList}
+				keyExtractor={(item) => `${item.name}`}
+				renderItem={renderItem}
+				showsVerticalScrollIndicator={false}
+			></FlatList>
+		);
 	};
 
 	function renderHeader() {
@@ -186,6 +368,8 @@ function GeneralChatScreen({ navigation }) {
 		);
 	}
 
+	
+
 	return (
 		<View
 			style={{
@@ -196,7 +380,7 @@ function GeneralChatScreen({ navigation }) {
 		>
 			{renderHeader()}
 			<View style={[{ flex: 1, marginTop: 10 }]}>
-				<ChatList chatsList={filteredData} navigation={navigation}></ChatList>
+				{renderChatList(filteredData)}
 			</View>
 		</View>
 	);
@@ -210,6 +394,18 @@ const styles = StyleSheet.create({
 	containerTxt: {
 		fontSize: 13,
 		fontWeight: "bold",
+	},
+	chatName: {
+		textAlign: "center",
+		fontSize: 15,
+		fontWeight: "bold",
+		color: COLORS.secondary,
+	},
+	chatLastMessage: {
+		fontSize: 13,
+		alignSelf: "flex-start",
+		color: COLORS.darkGrey,
+		marginHorizontal: 5,
 	},
 	shadow: {
 		shadowColor: COLORS.black,
