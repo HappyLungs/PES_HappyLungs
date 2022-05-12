0a41cfa46bfec607465c22b79f9dc60ae4e8dda7
juliahc <julia.herrera.caba@estudiantat.upc.edu>
2022-05-12 22:55:27 +0200

    task #234: Vista llistat usuaris

---
+++ src/web/domain/User.js
@@ -0,0 +1,15 @@
+class User {
+    
+    constructor(params) {
+        this.username = params.username;
+        this.email = params.email;
+        this.blocked = params.blocked;
+        this.pins = params.pins;
+        this.ranking = params.ranking;
+        this.chats = params.chats;
+        this.reported = params.reported;
+    }
+
+}
+
+module.exports = User;
--- src/web/routes/views.js
+++ src/web/routes/views.js
@@ -3,6 +3,32 @@ const router = express.Router();
 
 module.exports = router;
 
+//[GET] Main page
 router.get("/", async (req, res) => {
-    res.render("users");
+    res.render("users", {page:"users"});
+});
+
+//[GET] All users
+router.get("/users", async (req, res) => {
+    res.render("users", {page:"users"});
+});
+
+//[GET] Blocked users
+router.get("/blocked", async (req, res) => {
+    res.render("users", {page:"blocked"});
+});
+
+//[PUT] Block a user
+router.put("/blockUser/:userId", async (req, res) => {
+    
+});
+
+//[PUT] Block a user
+router.put("/unBlockUser/:userId", async (req, res) => {
+    
+});
+
+//[GET] Reported messages of a blocked user
+router.get("blocked/:userId", async (req, res) => {
+
 });
