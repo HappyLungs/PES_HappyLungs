98b1b27f356338730c397f639d5e7ca5379b6a15
IvanJimenoRamirez <ivan.jimeno@estudiantat.upc.edu>
2022-05-14 19:27:14 +0200

    Update list message

--- src/web/domain/controllers/MessageCtrl.js
+++ src/web/domain/controllers/MessageCtrl.js
@@ -20,9 +20,28 @@ let MessageCtrl;
 //[GET] reported messages
 MessageCtrl.prototype.fetchMessages = async function(id) {
     //DB get reported messages from user with email = id
-    let result = await this.db.getRequest("/listReportedMessages", {email: id});
-    if (result.status === 200) return result.data;
-    else return [];
+    let messagesData = await this.db.getRequest("/listReportedMessages", {email: id});
+    let userData = await this.db.getRequest("/user", {email: id});
+    let result = {};
+    let messages = [];
+    if (messagesData.status === 200 && userData.status === 200) {
+        for (let message of messagesData.data) {
+            let state = "";
+            if (message.state === -2) state = "accepted";
+            else if (message.state === -1) state = "declined";
+            else state = "pending";
+            let date = new Date(message.createdAt);
+            let messageObj = new Message({id: message._id, email: message.user, reportantUsername: message.reportantUsername, text: message.text, state: state, date: date.getDate(), hour: date.getHours});
+            messages.push(messageObj);
+        }
+        result.messages = messages;
+        result.user = {
+            username: user.data.name,
+            email: user.data.email,
+            picture: user.data.profilePicture
+        };
+        return result;
+    } else return [];
 }
 
 //[PUT] accept reported message
