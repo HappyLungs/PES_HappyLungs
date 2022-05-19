f7a2641b40e4a969df15e51024351fa9b4f19719
juliahc <julia.herrera.caba@estudiantat.upc.edu>
2022-05-19 15:16:48 +0200

    #task 202: conversations

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io").listen(server);
+const io = require("socket.io")(server);
 const port = 8000;
 
 io.on("connection", socket => {
