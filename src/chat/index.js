73dae30acb54af6cdcc1a1055ace8cff20aad11e
IvanJimenoRamirez <ivan.jimeno@estudiantat.upc.edu>
2022-05-21 10:34:42 +0200

    fix

--- src/chat/index.js
+++ src/chat/index.js
@@ -3,15 +3,19 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = 8000;
+let sockets = new Map();
 
 io.on("connection", socket => {
+<<<<<<< HEAD
     console.log("a user connected :D, id -> ", socket.id, socket.handshake.query.email);
+=======
+    console.log("a user connected :D, socket id -> ", socket.id);
+>>>>>>> parent of c1fb669 ( #task 202: conversations)
     
-    socket.on("chat message", info => {
-      console.log("Message: ", info.message);
-      console.log("To: ", info.to)
+    socket.on("chat message", ({msg, to}) => {
+      console.log("Message and to: ", msg, to);
       //io.to(msg.conversation).emit("chat message", msg);
-      io.emit("chat message", info.message);
+      io.emit("chat message", msg);
     });
 
     //console.log("Id: ",socket.handshake.query.id);
